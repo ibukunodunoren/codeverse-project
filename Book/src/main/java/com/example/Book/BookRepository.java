4e93923665c632bad4c25c1eb52a9370de27eb2d
bushrajav <bushra_azhar21@yahoo.com>
2023-03-23 13:41:09 +0000

    Search a book by book title

--- Book/src/main/java/com/example/Book/BookRepository.java
+++ Book/src/main/java/com/example/Book/BookRepository.java
@@ -1,6 +1,11 @@
 package com.example.Book;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BookRepository extends CrudRepository<Book, Integer> {
+    @Query(value = "SELECT * FROM BOOK WHERE TITLE ILIKE %?%", nativeQuery = true)
+    public List<Book> findByTitle(String title);
 }
--- Book/src/main/java/com/example/Book/MyRESTController.java
+++ Book/src/main/java/com/example/Book/MyRESTController.java
@@ -2,7 +2,10 @@ package com.example.Book;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MyRESTController {
@@ -13,6 +16,11 @@ public class MyRESTController {
     @GetMapping("/books")
     public Iterable<Book> getBooks() {
         return repository.findAll();
+    }
+
+    @GetMapping("/search")
+    public List<Book> getSearch(@RequestParam String title) {
+        return repository.findByTitle(title);
     }
 
 }
